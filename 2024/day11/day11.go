e3ded421e5d50b09ab9b1eb10b72fb1e73907c35
Dean Logan <deanlogan42@gmail.com>
2024-12-13 19:08:09 +0000

    Completed 2024 day 11 part 2

--- 2024/day11/day11.go
+++ 2024/day11/day11.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+    "fmt"
+    "strconv"
+    "sync"
 
-	"github.com/DeanLogan/advent-of-code/libs"
+    "github.com/DeanLogan/advent-of-code/libs"
 )
 
 func main(){
@@ -50,5 +51,56 @@ func blink(stones []int) []int {
 
 func part2(){
     ans := 0
+
+    stones := libs.StrToIntSlice(libs.AllFileContentAsString("2024/day11/input.txt"), " ")
+
+    ans = getNumStones(stones, 75)
+
     fmt.Println("The answer to part 2 for day 11 is:", ans)
+}
+
+func getNumStones(stones []int, times int) int {
+    wg := sync.WaitGroup{}
+    checked := &sync.Map{}
+    ch := make(chan int, len(stones))
+    for _, stone := range stones {
+        wg.Add(1)
+        go func(stone int) {
+            defer wg.Done()
+            ch <- getNumStonesAfter(stone, times, checked)
+        }(stone)
+    }
+    wg.Wait()
+    close(ch)
+    result := 0
+    for value := range ch {
+        result += value
+    }
+    return result
+}
+
+func getNumStonesAfter(stone, times int, checked *sync.Map) int {
+    result := 0
+    if times > 0 {
+        if v, ok := checked.Load(fmt.Sprintf("%d:%d", stone, times)); ok {
+            result += v.(int)
+        } else {
+
+            if stone == 0 {
+                result += getNumStonesAfter(1, times-1, checked)
+            } else if digits := strconv.Itoa(stone); len(digits)%2 == 0 {
+                a, _ := strconv.Atoi(digits[:len(digits)/2])
+                b, _ := strconv.Atoi(digits[len(digits)/2:])
+                result += getNumStonesAfter(a, times-1, checked)
+                result += getNumStonesAfter(b, times-1, checked)
+            } else {
+                result += getNumStonesAfter(stone*2024, times-1, checked)
+            }
+
+            checked.Store(fmt.Sprintf("%d:%d", stone, times), result)
+        }
+    } else {
+        return 1
+    }
+    return result
 }
