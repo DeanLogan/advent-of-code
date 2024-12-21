4cf451d4d920e7ec5555ad844b44edfad24fe636
Dean Logan <deanlogan42@gmail.com>
2024-12-21 14:17:39 +0000

    Completed 2024 day 16 part 2

--- 2024/day16/day16.go
+++ 2024/day16/day16.go
@@ -3,7 +3,6 @@ package main
 import (
     "fmt"
     "container/heap"
-    // "math"
 
     "github.com/DeanLogan/advent-of-code/libs"
 )
@@ -25,7 +24,7 @@ type Edge struct {
 }
 
 type Item struct { 
-	Pos Pos 
+	pos Pos 
 	priority int 
 	index int 
 } 
@@ -63,14 +62,16 @@ func (pq *PriorityQueue) Pop() interface{} {
 } 
 
 func part1(){
+	ans := 0
+
     maze := libs.FileToSlice("2024/day16/input.txt", "\n")
     startPos := getPosForChar(maze, 'S')
     endPos := getPosForChar(maze, 'E')
 
     weightedMaze := weightMaze(maze)
-    score := dijkstra(weightedMaze, startPos, endPos)
+    ans = dijkstra(weightedMaze, startPos, endPos)
 
-    fmt.Println("🎄 The answer to part 1 for day 16 is:", score, "🎄")
+    fmt.Println("🎄 The answer to part 1 for day 16 is:", ans, "🎄")
 }
 
 func weightMaze(maze []string) map[Pos][]Edge {
@@ -86,6 +87,7 @@ func weightMaze(maze []string) map[Pos][]Edge {
 			} 
 		}
 	}
+
 	return weightedMaze
 }
 
@@ -120,6 +122,7 @@ func determineEdges(maze []string, pos Pos) []Edge {
 
 func nextPosValid(maze []string, pos Pos) (bool, Pos) {
 	nextPos := pos
+
 	switch pos.dir {
 	case 0:
 		nextPos.y -= 1
@@ -130,8 +133,9 @@ func nextPosValid(maze []string, pos Pos) (bool, Pos) {
 	case 3:
 		nextPos.x -= 1
 	default:
-		
+		break
 	}
+
 	if (nextPos.y > 0 && nextPos.y < len(maze) && nextPos.x > 0 && nextPos.x < len(maze[0])) && (maze[nextPos.y][nextPos.x] == '.' || maze[nextPos.y][nextPos.x] == 'S' || maze[nextPos.y][nextPos.x] == 'E') { 
 		return true, nextPos
 	}
@@ -141,17 +145,21 @@ func nextPosValid(maze []string, pos Pos) (bool, Pos) {
 func dijkstra(weightedMaze map[Pos][]Edge, start Pos, end Pos) int { 
     distances := make(map[Pos]int) 
     previousNodes := make(map[Pos][]Pos) 
+
     for pos := range weightedMaze { 
         distances[pos] = int(^uint(0) >> 1) 
     } 
+
     distances[start] = 0 
     priorityQueue := make(PriorityQueue, 0) 
     heap.Init(&priorityQueue) 
-    heap.Push(&priorityQueue, &Item{ Pos: start, priority: 0, }) 
+    heap.Push(&priorityQueue, &Item{ pos: start, priority: 0, }) 
     visited := make(map[Pos]bool) 
+
     var endPos *Pos 
+
     for priorityQueue.Len() > 0 { 
-        currentPos := heap.Pop(&priorityQueue).(*Item).Pos 
+        currentPos := heap.Pop(&priorityQueue).(*Item).pos 
         if currentPos.x == end.x && currentPos.y == end.y { 
             endPos = &currentPos 
             break 
@@ -160,20 +168,24 @@ func dijkstra(weightedMaze map[Pos][]Edge, start Pos, end Pos) int {
             continue 
         } 
         visited[currentPos] = true 
+
         for _, edge := range weightedMaze[currentPos] { 
             if visited[edge.p] { 
                 continue 
             } 
+
             alternativeDist := distances[currentPos] + edge.score 
+			
             if alternativeDist < distances[edge.p] { 
                 distances[edge.p] = alternativeDist 
                 previousNodes[edge.p] = []Pos{currentPos} 
-                heap.Push(&priorityQueue, &Item{ Pos: edge.p, priority: alternativeDist, }) 
+                heap.Push(&priorityQueue, &Item{ pos: edge.p, priority: alternativeDist, }) 
             } else if alternativeDist == distances[edge.p] { 
                     previousNodes[edge.p] = append(previousNodes[edge.p], currentPos) 
             } 
         } 
     } 
+
     if endPos == nil { 
         return -1
     } 
@@ -194,5 +206,107 @@ func getPosForChar(maze []string, char rune) Pos {
 
 func part2(){
     ans := 0
-    fmt.Println("🎄 The answer to part 2 for day 16 is:", ans, "🎄")
+
+	maze := libs.FileToSlice("2024/day16/input.txt", "\n")
+    startPos := getPosForChar(maze, 'S')
+    endPos := getPosForChar(maze, 'E')
+
+    weightedMaze := weightMaze(maze)
+
+	bestPaths := dijkstra2(weightedMaze, startPos, endPos)
+	uniqueTiles := make(map[Pos]bool)
+
+	for _, path := range bestPaths {
+		for _, tile := range path {
+			uniqueTiles[Pos{x: tile.x, y: tile.y, dir:0}] = true
+		}
+	}
+
+	ans = len(uniqueTiles)
+
+    fmt.Println("🎄 The answer to part 2 for day 16 is:", ans,"🎄")
+}
+
+func dijkstra2(weightedMaze map[Pos][]Edge, start Pos, end Pos) [][]Pos { 
+    distances := make(map[Pos]int) 
+    previousNodes := make(map[Pos][]Pos) 
+
+    for pos := range weightedMaze { 
+        distances[pos] = int(^uint(0) >> 1) 
+    } 
+
+    distances[start] = 0 
+    priorityQueue := make(PriorityQueue, 0) 
+    heap.Init(&priorityQueue) 
+    heap.Push(&priorityQueue, &Item{ pos: start, priority: 0, }) 
+    visited := make(map[Pos]bool) 
+
+    var endPos *Pos 
+
+    for priorityQueue.Len() > 0 { 
+        currentPos := heap.Pop(&priorityQueue).(*Item).pos 
+        if currentPos.x == end.x && currentPos.y == end.y { 
+            endPos = &currentPos 
+            break 
+        } 
+        if visited[currentPos] { 
+            continue 
+        } 
+        visited[currentPos] = true 
+
+        for _, edge := range weightedMaze[currentPos] { 
+            if visited[edge.p] { 
+                continue 
+            } 
+
+            alternativeDist := distances[currentPos] + edge.score 
+
+            if alternativeDist < distances[edge.p] { 
+                distances[edge.p] = alternativeDist 
+                previousNodes[edge.p] = []Pos{currentPos} 
+                heap.Push(&priorityQueue, &Item{ pos: edge.p, priority: alternativeDist, }) 
+            } else if alternativeDist == distances[edge.p] { 
+                    previousNodes[edge.p] = append(previousNodes[edge.p], currentPos) 
+            } 
+        } 
+    } 
+
+    if endPos == nil { 
+        return nil 
+    } 
+    
+    paths := reconstructPaths(previousNodes, *endPos) 
+    return paths 
+}
+
+func reconstructPaths(previous map[Pos][]Pos, end Pos) [][]Pos { 
+	var paths [][]Pos 
+	var stack [][]Pos 
+
+	stack = append(stack, []Pos{end}) 
+	for len(stack) > 0 { 
+		path := stack[len(stack)-1] 
+		stack = stack[:len(stack)-1] 
+		cur := path[0] 
+
+		if len(previous[cur]) == 0 { 
+			paths = append(paths, path) 
+		} else { 
+			for _, prev := range previous[cur] { 
+				newPath := make([]Pos, len(path)+1) 
+				copy(newPath[1:], path) 
+				newPath[0] = prev 
+				stack = append(stack, newPath) 
+			} 
+		} 
+	} 
+	
+	for i, path := range paths { 
+		for j := 0; j < len(path)/2; j++ { 
+			path[j], path[len(path)-1-j] = path[len(path)-1-j], path[j] 
+		} 
+
+		paths[i] = path 
+	} 
+	return paths
 }
