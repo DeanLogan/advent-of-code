f7cb409abe512c04a5f8102b7c5da3243b0b243a
Dean Logan <deanlogan42@gmail.com>
2025-12-02 15:39:54 +0000

    Completed 2025 day 2 part 2

--- 2025/day02/day02.go
+++ 2025/day02/day02.go
@@ -18,7 +18,6 @@ func part1(){
     inputFile := libs.AllFileContentAsString("2025/day02/input.txt")
     idRanges := strings.Split(inputFile, ",")
     for _, idRangeStr := range idRanges {
-        fmt.Println(idRangeStr)
         idRange := libs.StrToIntSlice(idRangeStr, "-")
         ans += getInvalidIdTotalForRange(idRange[0],idRange[1])
     }
@@ -30,7 +29,6 @@ func getInvalidIdTotalForRange(lowerRange int, higherRange int) int {
     for i := lowerRange; i<higherRange+1; i++ {
         idStr := strconv.Itoa(i)
         if checkRepeatSequenceTwice(idStr) {
-            fmt.Println(idStr)
             invalidIds += i
         }
     }
@@ -48,5 +46,46 @@ func checkRepeatSequenceTwice(str string) bool {
 
 func part2(){
     ans := 0
+    inputFile := libs.AllFileContentAsString("2025/day02/input.txt")
+    idRanges := strings.Split(inputFile, ",")
+    for _, idRangeStr := range idRanges {
+        idRange := libs.StrToIntSlice(idRangeStr, "-")
+        ans += getInvalidIdTotalForRange2(idRange[0],idRange[1])
+    }
     fmt.Println("🎄 The answer to part 2 for day 02 is:", ans, "🎄")
+}
+
+func getInvalidIdTotalForRange2(lowerRange int, higherRange int) int {
+    invalidIds := 0
+    for i := lowerRange; i<higherRange+1; i++ {
+        idStr := strconv.Itoa(i)
+        if repeatingPatternFactors(idStr) {
+            invalidIds += i
+        }
+    }
+    return invalidIds
+}
+
+func repeatingPatternFactors(str string) bool {
+    // checking if all of the characters are the same ensuring it is only counted once
+    if checkPatternMatch(str, 1, len(str)) {
+        return true
+    }
+    factorPairs := libs.PrimeFactorPairs(len(str))
+    factorPairs = factorPairs[1:]
+    for _, factorPair := range factorPairs {
+        if checkPatternMatch(str, factorPair[0], factorPair[1]) {
+            return true
+        }
+        if checkPatternMatch(str, factorPair[1], factorPair[0]) {
+            return true
+        }
+    }
+    return false
+}
+
+func checkPatternMatch(str string, patternLen int, expectedCount int) bool {
+    subString := str[:patternLen]
+    count := strings.Count(str, subString)
+    return count == expectedCount && count >= 2
 }
