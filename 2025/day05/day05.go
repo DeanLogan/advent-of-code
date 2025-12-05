d5a828bf9a8a5212af070970fe8c304293154251
Dean Logan <deanlogan42@gmail.com>
2025-12-05 18:23:47 +0000

    Completed 2025 day 5 part 1

---
+++ 2025/day05/day05.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+    "fmt"
+    "sort"
+    "strconv"
+    "strings"
+
+    "github.com/DeanLogan/advent-of-code/libs"
+)
+
+func main(){
+    part1()
+    part2()
+}
+
+type Range struct {
+    start int
+    end int
+}
+
+func part1(){
+    ans := 0
+    fileSections := libs.FileToSlice("2025/day05/input.txt", "\n\n")
+    rangesStr := strings.Split(fileSections[0], "\n")
+    ranges := createRangeArray(rangesStr)
+
+    sort.Slice(ranges, func(i, j int) bool {
+        return ranges[i].start < ranges[j].start
+    })
+
+    ranges = mergeRanges(ranges)
+    
+    ids := libs.StrToIntSlice(fileSections[1], "\n")
+    for _, id := range ids {
+        if checkIdInRanges(id, ranges) {
+            ans++
+        }
+    }
+    fmt.Println("🎄 The answer to part 1 for day 05 is:", ans, "🎄")
+}
+
+func createRangeArray(rangesStr []string) []Range {
+    ranges := []Range{}
+    for _, rangeStr := range rangesStr {
+        startstr, endStr := libs.SplitAtChar(rangeStr, '-')
+        start, _ := strconv.Atoi(startstr)
+        end, _ := strconv.Atoi(endStr)
+        ranges = append(ranges, Range{start, end})
+    }
+    return ranges
+}
+
+func mergeRanges(ranges []Range) []Range {
+    newRanges := []Range{}
+    for i := range ranges {
+        if i<len(ranges)-1 && ranges[i].end >= ranges[i+1].start {
+            ranges[i+1] = Range{ranges[i].start, libs.Max(ranges[i].end, ranges[i+1].end)}
+        } else {
+            newRanges = append(newRanges, ranges[i])
+        }
+    }
+    return newRanges
+}
+
+func checkIdInRanges(id int, ranges []Range) bool {
+    for _, idRange := range ranges {
+        if id >= idRange.start && id <= idRange.end {
+            return true
+        }
+    }
+    return false
+}
+
+func part2(){
+    ans := 0
+    fmt.Println("🎄 The answer to part 2 for day 05 is:", ans, "🎄")
+}
