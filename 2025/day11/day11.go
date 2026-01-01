548650c843d37c15dba89db4513165335078a7ab
Dean Logan <deanlogan42@gmail.com>
2026-01-01 17:57:26 +0000

    Completed 2025 day 11 part 2

--- 2025/day11/day11.go
+++ 2025/day11/day11.go
@@ -47,6 +47,43 @@ func countPaths(current string, visited map[string]bool, graph map[string][]stri
 }
 
 func part2(){
-    ans := 0
+    lines := libs.FileToSlice("2025/day11/input.txt", "\n")
+    serverMap := getServerMap(lines)
+    
+    svrToFft := pathsDP(serverMap, "svr", "fft")
+    fftToDac := pathsDP(serverMap, "fft", "dac")
+    dacToOut := pathsDP(serverMap, "dac", "out")
+    
+    ans := svrToFft * fftToDac * dacToOut
     fmt.Println("🎄 The answer to part 2 for day 11 is:", ans, "🎄")
+}
+
+func pathsDP(graph map[string][]string, start, end string) int {
+    dp := make(map[string]int)
+    for node := range graph {
+        dp[node] = 0
+    }
+    dp[end] = 1
+    
+    visited := make(map[string]bool)
+    visited[end] = true
+    
+    return dfsDP(graph, dp, visited, start)
+}
+
+func dfsDP(graph map[string][]string, dp map[string]int, visited map[string]bool, curr string) int {
+    if visited[curr] {
+        return dp[curr]
+    }
+    
+    visited[curr] = true
+    
+    for _, node := range graph[curr] {
+        if !visited[node] {
+            dfsDP(graph, dp, visited, node)
+        }
+        dp[curr] += dp[node]
+    }
+    
+    return dp[curr]
 }
